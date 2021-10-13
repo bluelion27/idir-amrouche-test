78310fd8f373cb85114df88770aa538791b2e8ba
Idir AMROUCHE <idiramrouche7@gmail.com>
2021-10-13 12:02:08 +0200

    Updated unit test

--- src/test/java/com/sipios/refactoring/controller/ShoppingControllerTests.java
+++ src/test/java/com/sipios/refactoring/controller/ShoppingControllerTests.java
@@ -4,14 +4,19 @@ import com.sipios.refactoring.UnitTest;
 import com.sipios.refactoring.model.Body;
 import com.sipios.refactoring.model.CustomerType;
 import com.sipios.refactoring.model.Item;
+import com.sipios.refactoring.service.ShoppingService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 
 class ShoppingControllerTests extends UnitTest {
 
     @InjectMocks
     private ShoppingController controller;
+
+    @Mock
+    private ShoppingService shoppingService;
 
     @Test
     void should_not_throw() {
